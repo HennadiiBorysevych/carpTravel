875fe0a9ff2f96d6ed496e6123db038e0184c868
Hennadii Borysevych <genuch3@gmail.com>
2023-08-19 21:16:08 +0200

    feat:logo partky

---
+++ components/Logo.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="w-[61px] h-[33px] duration-300 hover:opacity-60 focus:opacity-60 cursor-pointer">
+      <Link href="/" aria-label="CarpTravel logo">
+      </Link>
+    </div>
+  );
+}
--- sections/header/Header.jsx
+++ sections/header/Header.jsx
@@ -1,4 +1,11 @@
-const Header = () => {
-  return <header className="">s</header>;
-};
-export default Header;
+import Logo from "@/components/Logo";
+
+export default function Header() {
+  return (
+    <header className="absolute top-0 left-0 right-0 z-10 pt-[36px] md:pt-[25px]">
+      <div className="container flex items-center justify-between">
+        <Logo />
+      </div>
+    </header>
+  );
+}
